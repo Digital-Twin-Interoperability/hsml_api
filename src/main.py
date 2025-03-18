486bc5f452e1f409edee1dd4ffc5e1c079f061d9
Alicia Sanjurjo <100428537@alumnos.uc3m.es>
2025-03-18 12:03:21 -0700

    Changes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI, HTTPException, Depends
 import uvicorn
+from registration_api import router as registration_router
+#from registration_api import functions
+#from authentication_kafka_producer import functions
+#from authorization_kafka_consumer import functions
+
 
 app = FastAPI()
+
+# Include registration router (with base route like /register)
+app.include_router(registration_router, prefix="/register", tags=["registration"])
 
 
 if __name__ == "__main__":
